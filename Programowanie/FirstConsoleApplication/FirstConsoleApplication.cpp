ddcb9a564b57d983f792413d42a0dfa858da7af8
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-11 09:47:46 +0200

    11.10.2023

--- Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -6,8 +6,17 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hel\tlo World!\n"; // sta³a lancuchowa od 0 do 4 miliardow znaków
+
+	std::cout << "Nazywam sie Man Jajka\n";
+	std::cout << "Nazywam sie Man JaJka\n";
+	std::cout << 'X'; // sta³a znakowa- musi byc jeden znak
+	std::cout << "\n";
+	std::cout << '\n';
+	std::cout << 257 << "\n";//sta³a liczbowa ca³kowita
+	std::cout << 12.5 << "\n";//sta³a liczba rzeczywista
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
@@ -16,6 +25,7 @@ int main()
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
+//   4. Use the Error List window to view errors  
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
